0086a8d6bab749b5b9bc853e281f7fd16933f813
Nicky <92900519+Herecore@users.noreply.github.com>
2022-10-16 15:21:57 +0000

    removed the comment with task

--- Chapter 4/program.cpp
+++ Chapter 4/program.cpp
@@ -1,9 +1,3 @@
-/* 
-Add a unit to each double entered; that is, enter values such as 10cm, 2.5in, 5ft, 
-or 3.33m. Accept the four units: cm, m, in, ft. Assume conversion factors 1m == 100cm, 
-1in == 2.54cm, 1ft == 12in. Read the unit indicator into a string. You may consider 
-12 m (with a space between the number and the unit) equivalent to 12m (without a space).
-*/
 #include "std_lib_facilities.h"
 
 int main()
@@ -158,5 +152,3 @@ int main()
 
     cout << "\nExiting...\n\n";
 }
-
-
