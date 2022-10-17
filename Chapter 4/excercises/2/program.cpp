7f20eefd2d80314c87e4ec3cf34115c5ccf088d1
herecore <herecore@protonmail.com>
2022-10-17 13:51:08 +0300

    Solved the Excercise 2

--- Chapter 4/excercises/2/program.cpp
+++ Chapter 4/excercises/2/program.cpp
@@ -16,7 +16,13 @@ int main()
         sum += x;
 
     cout << "Average temperature: " << sum / double(temps.size()) << '\n';
+    
 
     sort(temps);
-    cout << "Median temperature: " << temps[int(temps.size()) / 2] << '\n';
+    double median = temps[int(temps.size()) / 2];
+    if (temps.size() % 2 == 0) {
+        int tmp = int(temps.size()) / 2;
+        median = (temps[tmp - 1] + temps[tmp]) / 2; 
+    }
+    cout << "Median temperature: " << median << '\n';
 }
