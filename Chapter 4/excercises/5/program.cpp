3c459f907813abf5682fee3baf7e3d82f3e6a53b
herecore <herecore@protonmail.com>
2022-10-17 19:27:29 +0300

    Solved exercise 5

--- Chapter 4/excercises/5/program.cpp
+++ Chapter 4/excercises/5/program.cpp
@@ -3,41 +3,32 @@
 
 int main()
 {
-    cout << "Welcome to simple calculator! It is able to do basic math "
-        << "operations with 2 numbers. \nSupported operations include:\n "
-        << "\t**Operation**\t\t**Code you'll have to enter**\n"
-        << "\tAddition\t\t\t+ OR plus\n"
-        << "\tSubstraction\t\t\t- OR minus\n"
-        << "\tMultiplication\t\t\t* OR mul\n"
-        << "\tDivision\t\t\t/ OR div\n"
-        << "CTRL+C to exit...\n";
-
     double num1, num2;
     string oper = "0";
 
     while (true) {
-        cout << "Enter the first number, operation code, and a second number "
+        cout << "Enter the first number, second number and operation "
             << "(space-separated): ";
-        cin >> num1 >> oper >> num2;
+        cin >> num1 >> num2 >> oper;
 
         // Addition
-        if (oper == "+" or oper == "plus")
-            cout << num1 << " plus " << num2 << " equals " 
+        if (oper == "+")
+            cout << "The sum of " << num1 << " and " << num2 << " is " 
                 << num1 + num2 << endl;
 
         // Substraction
-        if (oper == "-" or oper == "minus")
-            cout << num1 << " minus " << num2 << " equals " 
+        if (oper == "-")
+            cout << "The difference of " << num1 << " and " << num2 << " is " 
                 << num1 - num2 << endl;
 
         // Multiplication
-        if (oper == "*" or oper == "mul")
-            cout << num1 << " times " << num2 << " equals " 
+        if (oper == "*")
+            cout << "The product of " << num1 << " and " << num2 << " is " 
                 << num1 * num2 << endl;
 
         // Division
-        if (oper == "/" or oper == "div")
-            cout << num1 << " divided by " << num2 << " equals " 
+        if (oper == "/")
+            cout << "The quotient of " << num1 << " and " << num2 << " is " 
                 << num1 / num2 << endl;
     }
 }
